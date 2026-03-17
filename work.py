da8dcc87120a42d11d22dfd0e404ac07b0797453
dhakshithaboyapati03 <2411it010058@mallareddyuniversity.ac.in>
2026-03-17 21:41:57 +0530

    Update work.py

--- work.py
+++ work.py
@@ -1,5 +1,5 @@
-name = input("Enter your name: ")
-skill = input("Enter your main interest (technology / biology / business / creativity): ")
+name = "Dhakshitha"
+skill = "technology"
 
 print("\nAI Career Guidance Result for", name)
 
@@ -11,25 +11,3 @@ if skill == "technology":
     print("\nRecommended Career: Data Scientist")
     print("Suitable Stream: MPC")
     print("Courses: B.Sc Data Science, Machine Learning")
-
-elif skill == "biology":
-    print("\nRecommended Career: Doctor")
-    print("Suitable Stream: BiPC")
-    print("Courses: MBBS")
-
-    print("\nRecommended Career: Biotechnologist")
-    print("Suitable Stream: MPC or BiPC")
-    print("Courses: B.Sc Biotechnology")
-
-elif skill == "business":
-    print("\nRecommended Career: Business Analyst")
-    print("Suitable Stream: Commerce")
-    print("Courses: BBA, MBA")
-
-elif skill == "creativity":
-    print("\nRecommended Career: Graphic Designer")
-    print("Suitable Stream: Any Stream")
-    print("Courses: Graphic Design, UI/UX Design")
-
-else:
-    print("Please enter a valid interest.")
